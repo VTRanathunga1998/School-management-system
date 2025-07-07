8729383cbca71e9fec6a26a157e1da0f735955b2
Viraj Tharuka Ranathunga <viraj@jwareautomation.com>
2025-07-07 11:23:10 +0530

    responsive menu design

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,13 +1,24 @@
+import Menu from "@/components/Menu";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex h-screen">
+    <div className="flex">
       {/*LEFT*/}
-      <div className="W-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-        L
+      <div className="W-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">SUSL</span>
+        </Link>
+        <Menu />
       </div>
       {/*RIGHT*/}
       <div className="W-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
