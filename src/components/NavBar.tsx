505aeb855045babe364f1cfe4d227102af2a7ba5
Viraj Tharuka Ranathunga <viraj@jwareautomation.com>
2025-09-12 14:29:53 +0530

    make role dymanim

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 
-const NavBar = () => {
+const NavBar = async () => {
+  const user = await currentUser();
+
+  const role = user?.publicMetadata.role as string;
+
   return (
     <div className="flex items-center justify-between p-4">
       {/*SEARCH BAR*/}
@@ -31,7 +36,7 @@ const NavBar = () => {
         </div>
         <div className="flex flex-col">
           <span className="text-sm leading-3 font-medium">John Doe</span>
-          <span className="text-[10px] text-gray-500 text-right">Admin</span>
+          <span className="text-[10px] text-gray-500 text-right">{role}</span>
         </div>
         {/* <Image
           src="/avatar.png"
